986c807e74dd511d52d0adc78b1e0f7788a6b098
zucca <info@stackhouse.it>
2021-05-11 13:03:29 +0200

    fix dts

--- packages/box-core/index.d.ts
+++ packages/box-core/index.d.ts
@@ -2,13 +2,17 @@ import { Component } from 'react'
 
 declare module '@stackhouseos/box-core' {
     export type BoxProps = {
+        prefix: string;
+        fields: BoxField | BoxField[];
+    }
+    
+    export type BoxField = Partial<BoxProps> & {
+        type:string;
         rules?: {} | [];
         validate?: {};
         id?: string;
-        container?: BoxProps | string;
-        prefix?: string;
+        container?: BoxField | string;
         ruleModeDisable?: boolean;
-        fields: BoxProps | BoxProps[];
     }
 
     export default class Box extends Component<BoxProps> {
