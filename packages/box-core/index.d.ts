e8f5d6283e5c2d6b31327643ce79f5666cec7250
zucca <info@stackhouse.it>
2021-05-11 12:04:42 +0200

    add dts

--- packages/box-core/index.d.ts
+++ packages/box-core/index.d.ts
@@ -1,12 +1,19 @@
 import { Component } from 'react'
 
 declare module '@stackhouseos/box-core' {
-    type BoxProps = {
-        prefix: string
-        fields: Record<string, any>
+    export type BoxProps = {
+        rules?: {} | [];
+        validate?: {};
+        id?: string;
+        prefix?: string;
+        ruleModeDisable?: boolean;
+        fields: BoxProps | BoxProps[];
+        container: BoxProps | string;
     }
 
     export default class Box extends Component<BoxProps> {
         static extendControls ( controls: {}): Component
     }
+
+    export const createBoxInstance: () => Box;
 }
